8df3dfe97d6239825dd87381524176a6790d1a88
Daniel Gagnon <daniel.gagnon@logilynx.ca>
2018-08-30 10:28:10 -0400

    CCES-2.8.1

--- ProgramToFlash.bat
+++ ProgramToFlash.bat
@@ -8,7 +8,7 @@ REM pushd %~dp0
 set TargetName=BF707_Wagner
 set Configuration=Debug
 set TargetExt=.ldr
-set CCESDir=d:\Analog Devices\CrossCore Embedded Studio 2.8.0
+set CCESDir=d:\Analog Devices\CrossCore Embedded Studio 2.8.1
 set DriverPath=d:\Analog Devices\ADSP-BF707_EZ-Board-Rel1.0.1\BF707_EZ-Board\Blackfin\Examples\Device_programmer\bf707_w25q32bv_dpia.dxe
 
 ECHO Programming %TargetName% to SPI Flash
