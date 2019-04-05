60311a7106d2694fd299a209772cd5f0cfc87c5e
JeanAlexisBoulet <ja.boulet@phantomintelligence.com>
2019-04-05 14:31:59 -0400

    Updated project file

--- demo_app/ProgramToFlash.bat
+++ demo_app/ProgramToFlash.bat
@@ -10,9 +10,11 @@ set Configuration=Debug
 set TargetExt=.ldr
 set CCESDir=c:\Analog Devices\CrossCore Embedded Studio 2.8.1
 set DriverPath=D:\Projects\Wagner\flash_programmer.dxe
+set configFile=wagner_custom_board_support.xml
 
 ECHO Programming %TargetName% to SPI Flash
 
+REM "%CCESDir%\cldp.exe" -config %configFile% -emu 1000 -driver "%DriverPath%" -cmd prog -erase affected -format hex -file %Configuration%\%TargetName%%TargetExt% -verbose -log
 "%CCESDir%\cldp.exe" -proc ADSP-BF707 -emu 1000 -driver "%DriverPath%" -cmd prog -erase affected -format hex -file %Configuration%\%TargetName%%TargetExt% -verbose -log 
 REM "%CCESDir%\cldp.exe" -verbose -log -proc ADSP-BF707 -emu 1000 -driver "%DriverPath%" -cmd save -format hex -numbyte 1024 -file d:\savemem5.ldr
 REM "%CCESDir%\cldp.exe" -verbose -log -proc ADSP-BF707 -emu 1000 -driver "%DriverPath%" -cmd compare -file %Configuration%\%TargetName%%TargetExt% -format hex 
