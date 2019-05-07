361c428e6c1468d9dee056b519d6a33b4fb1fa26
JeanAlexisBoulet <ja.boulet@phantomintelligence.com>
2019-05-07 12:09:50 -0400

    moved Program To flash to the rigth location

---
+++ ProgramToFlash.bat
@@ -0,0 +1,20 @@
+REM @echo off
+
+REM IF "%1"=="" GOTO SKIP
+REM IF "%2"=="" GOTO SKIP
+
+REM pushd %~dp0
+
+set TargetName=demo_app
+set Configuration=Debug
+set TargetExt=.ldr
+set CCESDir=c:\Analog Devices\CrossCore Embedded Studio 2.8.3
+set DriverPath=D:\Projects\Wagner\flash_programmer\flash_programmer.dxe
+set configFile=wagner_custom_board_support.xml
+
+ECHO Programming %TargetName% to SPI Flash
+
+"%CCESDir%\cldp.exe" -proc ADSP-BF707 -emu 1000 -driver "%DriverPath%" -cmd prog -erase affected -format hex -file %Configuration%\%TargetName%%TargetExt% -verbose -log 
+REM popd
+
+:DONE
