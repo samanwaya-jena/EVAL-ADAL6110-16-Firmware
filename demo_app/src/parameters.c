f9410b07780b2a3e31e3df811ff9edc03cf34b0a
Eric Turenne <e.turenne@phantomintelligence.com>
2020-02-22 18:38:04 -0500

    added parameters handler file

---
+++ demo_app/src/parameters.c
@@ -0,0 +1,8 @@
+/*
+ * parameters.c
+ *
+ *  Created on: Feb 22, 2020
+ *      Author: e.turenne
+ */
+
+
---
+++ demo_app/src/parameters.h
@@ -0,0 +1,13 @@
+/*
+ * parameters.h
+ *
+ *  Created on: Feb 22, 2020
+ *      Author: e.turenne
+ */
+
+#ifndef SRC_PARAMETERS_H_
+#define SRC_PARAMETERS_H_
+
+
+
+#endif /* SRC_PARAMETERS_H_ */
